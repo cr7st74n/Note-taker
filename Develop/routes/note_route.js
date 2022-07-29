235281b9deac768342564b984cfa8e9b44ab1bcb
cr7st74n <cris-082011@hotmail.com>
2022-07-29 09:42:11 -0500

    fixing error JSON in index.js

--- Develop/routes/note_route.js
+++ Develop/routes/note_route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const fs = require("fs");
 const path = require("path");
+const {v4:uuid} =require("uuid");
 const dbPath = path.join(__dirname,"../db/db.json");
 
 function getNotes(){
@@ -9,10 +10,12 @@ function getNotes(){
     .then(data => JSON.parse(data));
 }
 
+//Get our dataBase notes 
 router.get("/notes", (req,res)=>{
     getNotes()
     .then(data_note =>{
         res.json(data_note);
+        console.log("data recived");
     })
     .catch((err)=>console.log(err));
 })
@@ -21,10 +24,37 @@ router.post("/notes",(req,res)=>{
     getNotes()
     .then((data_note)=>{
         const this_data = req.body;
-        const Id_data = data_note.length ? this_data[data_note.length -1].id:0;
-    })
 
-})
+        this_data.id = uuid().slice(0,6);
+        data_note.push(this_data);
+
+        fs.promises.writeFile(dbPath, JSON.stringify(this_data,null,2))
+        .then(()=>{
+            console.log('too bien');
+            res.json(this_data);
+        }).catch(err => console.log(err));
+
+    })
+});
+
+router.delete("/notes/:id",(req,res)=>{
+    getNotes()
+    .then(note_del =>{
+
+        let id = req.params.id;
+    
+        // filtering out object from array
+        let NewNoteData = note_del.filter(currItem => currItem.id != id);
+
+        fs.promises.writeFile(dbPath, JSON.stringify(NewNoteData,null,1))
+        .then(()=>{
+            console.log('deleted succeddfully');
+            res.json(NewNoteData);
+
+        })
+        .catch(err => console.log(err));
+    })
+});
 
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const note_router = require("./routes/note_route");
+const api_route = require("./routes/api_routes");
 const server = express();
 const PORT = process.env.PORT || 3330;
 const path = require("path");
@@ -8,12 +9,15 @@ const path = require("path");
 
 //Share Static/Browser Files
 server.use(express.static(path.join(__dirname,"public")));
-// Attach client 
 
+//Very import to set this middleware Detect the form data !
+// see the data from our Front End 
 server.use(express.urlencoded({extended: true}))
 server.use(express.json());
 //Load Routers
+
 server.use("/api",note_router);
+server.use("/notes", api_route);
 
 
 server.listen(PORT, ()=>{
